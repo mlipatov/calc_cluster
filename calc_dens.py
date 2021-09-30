fixed an error in the normalization of the age prior; generalized this normalization to uneven age grids; refined the age grid more; made other small changes

calc_dens.py
@@ -107,7 +107,7 @@ for it in range(len(filelist)):
 				# check that the kernel, evaluated at the ROI boundaries, fits within the grid
 				if density.check_roi(i, kernel): 
 					density.convolve(i, kernel, ds=cf.downsample) # convolve
-			# normalize
+			# normalize so that the density adds up to 1 over the RON
 			density.normalize() 
 			# calculate the dependence of probability change on standard deviation of further convolving kernel
 			density.dP_sigma(nsig)
@@ -149,7 +149,9 @@ for it in range(len(filelist)):
 				else: 						density1 = densities[j][k] # vsini > v_0 
 				# integration with the kernel, which is normalized up to the product of step sizes
 				dens = np.sum(kernels[i] * density1.dens[slices[i]])
-				dens /= np.prod(density1.step) # scale by density step sizes
+				# scale by density step sizes; 
+				# this normalizes the density to integrate to 1 over the RON, instead of summing to 1
+				dens /= np.prod(density1.step) 
 				# normalization correction for this data point in this density grid
 				norm = 1.
 				for d in range(density1.dim):

calc_like.py
@@ -39,6 +39,8 @@ for filepath in filelist:
 			m = ~np.isin(t1, t) # where the new ages are not in exisiting ages
 			points = np.concatenate((points, pts1[m]), axis=0)
 			t = np.concatenate((t, t1[m]))
+# age steps for mid-point Riemann sum integration; dimension: age
+delta_t = np.concatenate((np.array([t[1] - t[0]]), (t[2:] - t[:-2])/2, np.array([t[-1] - t[-2]])))
 
 # background probability density
 back = np.empty(npts)
@@ -80,22 +82,23 @@ else:
 
 	print( 'age means: ' + ', '.join(['%.4f' % t for t in t_mean]) )
 	print( 'age standard deviations: ' + ', '.join(['%.4f' % t for t in t_sigma]) )
-	print( 'data point age grid step: ' + '%.5f' % (t[1] - t[0]) )
+	print( 'average age grid step: ' + '%.5f' % np.mean(delta_t) )
 	print( 'means grid step: ' + '%.5f' % (t_mean[1] - t_mean[0]) )
 	print( 'deviations grid step: ' + '%.5f' % (t_sigma[1] - t_sigma[0]) )
 	print( 'slow rotator proportion step: ' + '%.5f' % (cf.w0[1] - cf.w0[0]) )
-	print( 'fast rotator proportion step: ' + '%.5f' % (cf.w1[1] - cf.w1[0]) )
+	print( 'fast rotator proportion step: ' + '%.5f' % (cf.w2[1] - cf.w2[0]) )
 
-	# construct age priors, normalized up to the same factor
+	# construct the normalized age priors
 	# dimensions: age, prior mean, prior sigma
 	t_pr = np.exp( -0.5 * (t[:, np.newaxis, np.newaxis] - t_mean[np.newaxis, :, np.newaxis])**2 \
 							 / t_sigma[np.newaxis, np.newaxis, :]**2 )
+	# assume integration is Riemann, with the given delta ages; multiply by the deltas
+	t_pr *= delta_t[:, np.newaxis, np.newaxis]
+	# normalize so that the sum is equal to 1
 	t_pr /= np.sum(t_pr, axis=0)
 	# marginalize probability densities at data point locations by the age prior,
-	# correct by age step if the densities have to be normalized to 1; assume age step size is constant
 	# dimensions of output: age prior mean, age prior sigma, multiplicity, rotational population, data point 
-	ft = np.sum(t_pr[..., np.newaxis, np.newaxis, np.newaxis] * points[:, np.newaxis, np.newaxis, ...], axis=0) \
-							/ (t[1] - t[0])
+	ft = np.sum(t_pr[..., np.newaxis, np.newaxis, np.newaxis] * points[:, np.newaxis, np.newaxis, ...], axis=0)
 	t0_ar = t_mean # the first age-related parameter is the mean of the distribution
 	t1_ar = t_sigma # the second parameter is the standard deviation
 
@@ -179,7 +182,7 @@ def ds(l, n):
 	return x
 
 ## marginalize in q under uniform prior for combinations of population proportions and age prior parameters
-ll = np.full( (len(cf.w0), len(cf.w1), len(t0_ar), len(t1_ar)), np.nan )
+ll = np.full( (len(cf.w0), len(cf.w2), len(t0_ar), len(t1_ar)), np.nan )
 qm = np.full_like( ll, np.nan ) # array of maximum-likelihood q
 bm = np.full_like( ll, np.nan ) # array of maximum-likelihood b
 
@@ -222,13 +225,13 @@ while run < 2:
 			f = ft[it0, it1]
 			for i in ds(len(cf.w0), n):
 				w0 = cf.w0[i]
-				j_range = np.searchsorted(cf.w1, 1 - w0, side='right')
+				j_range = np.searchsorted(cf.w2, 1 - w0, side='right')
 				for j in ds(j_range, n):
-					w1 = cf.w1[j]
+					w2 = cf.w2[j]
 					# unary probability densities for all data points
-					f0 = f[0, 0, :] * w0 + f[0, 1, :] * (1 - w0 - w1) + f[0, 2, :] * w1
+					f0 = f[0, 0, :] * w0 + f[0, 1, :] * (1 - w0 - w2) + f[0, 2, :] * w2
 					# binary probability densities for all data points
-					f1 = f[1, 0, :] * w0 + f[1, 1, :] * (1 - w0 - w1) + f[1, 2, :] * w1
+					f1 = f[1, 0, :] * w0 + f[1, 1, :] * (1 - w0 - w2) + f[1, 2, :] * w2
 					# coefficients of q and q * b in the likelihood factors (the remaining term is 1)
 					A = f0 / back - 1
 					B = (f1 - f0) / back
@@ -294,7 +297,7 @@ while run < 2:
 							b1n = max(b1n, b1new)
 					elif run == 1: # if this is the final run
 						# update the global highest likelihood 
-						# if the highest likelihood at these tm, ts, w0 and w1 is higher;
+						# if the highest likelihood at these tm, ts, w0 and w2 is higher;
 						# also update the maximum-likelihood factors of individual data points here
 						qi, bi = np.unravel_index(np.nanargmax(l), l.shape)
 						llmax = np.log(l[qi, bi]) + ll_corr
@@ -349,13 +352,13 @@ while run < 2:
 	print('next run code: ' + str(run), flush=True)
 
 # print('marginalization in q on a grid of w_0, w_1 and b: ' + '%.2f' % (time.time() - start) + ' seconds.')
-w0i, w1i, t0i, t1i = np.unravel_index(np.nanargmax(ll),ll.shape)
+w0i, w2i, t0i, t1i = np.unravel_index(np.nanargmax(ll),ll.shape)
 print('max ln marginalized likelihood: ' + str(np.nanmax(ll)) + ' at w_0 = ' + '%.4f' % cf.w0[w0i] + \
-	', 1 - w_0 - w_1 = ' + '%.4f' % (1 - cf.w0[w0i] - cf.w1[w1i]) + ', w_1 = ' + '%.4f' % cf.w1[w1i] +\
-	', t0_ar = ' + '%.4f' % t0_ar[t0i] + ', t1_ar = ' + '%.4f' % t1_ar[t1i])
+	', w_1 = ' + '%.4f' % (1 - cf.w0[w0i] - cf.w2[w2i]) + ', w_2 = ' + '%.4f' % cf.w2[w2i] +\
+	', t0_ar (e.g. mu_t) = ' + '%.4f' % t0_ar[t0i] + ', t1_ar (e.g. sigma_t) = ' + '%.4f' % t1_ar[t1i])
 print('min ln marginalized likelihood: ' + str(np.nanmin(ll)))
-print('max ln likelihood: ' + '%.4f' % LLmax + ', at q = ' + '%.4f' % qm[w0i, w1i, t0i, t1i] + \
-	' and b = ' +  '%.4f' % bm[w0i, w1i, t0i, t1i])
+print('max ln likelihood: ' + '%.4f' % LLmax + ', at q = ' + '%.4f' % qm[w0i, w2i, t0i, t1i] + \
+	' and b = ' +  '%.4f' % bm[w0i, w2i, t0i, t1i])
 
 suffix = str(cf.Z).replace('-', 'm').replace('.', 'p') + \
 	'_os' + '_'.join([('%.2f' % n).replace('.','') for n in cf.om_sigma]) + '.pkl'
@@ -364,10 +367,10 @@ suffix = str(cf.Z).replace('-', 'm').replace('.', 'p') + \
 # package the likelihoods, the ML q values and the rotational distribution standard deviations
 # replace cf.om_sigma with om_sigma when these are inherited
 with open(like_dir + 'pkl/ll_' + suffix, 'wb') as f:
-	pickle.dump([ll, qm, bm, t0_ar, t1_ar, cf.w0, cf.w1, cf.om_sigma], f)
+	pickle.dump([ll, qm, bm, t0_ar, t1_ar, cf.w0, cf.w2, cf.om_sigma], f)
 
 # package the likelihood factors of individual data points with the corresponding cluster model parameters
-with open(like_dir + 'pkl/	lf_' + suffix, 'wb') as f:
+with open(like_dir + 'pkl/lf_' + suffix, 'wb') as f:
 	pickle.dump([LF_max, \
-		qm[w0i, w1i, t0i, t1i], bm[w0i, w1i, t0i, t1i], \
-		t0_ar[t0i], t1_ar[t1i], cf.w0[w0i], cf.w1[w1i], cf.om_sigma], f)
+		qm[w0i, w2i, t0i, t1i], bm[w0i, w2i, t0i, t1i], \
+		t0_ar[t0i], t1_ar[t1i], cf.w0[w0i], cf.w2[w2i], cf.om_sigma], f)

calc_obs.py
@@ -53,7 +53,7 @@ elif ages == 2:
 
 lt = 5; splits = [lt] * (nt - 1)  # number of ages for each interval to give linspace
 t = np.unique(st.t)[it : it + nt] # ages around 9.159
-st.select(np.isin(st.t, t)) # select the ages
+st.select(np.isin(st.t, t)) # select the ages in the model set
 
 # check that initial masses aren't multi-valued at constant (EEP, omega0, age)
 EEP = np.unique(st.EEP)
@@ -71,6 +71,11 @@ ts = [np.linspace(t[i], t[i+1], splits[i]) for i in range(nt - 1)]
 t_orig = [True]
 for i in range(nt - 1): t_orig = [True] + [False]*(lt - 2) + t_orig
 t = np.unique(np.concatenate(ts)) # refined ages
+# further refine the age grid:
+# split the first 7 intervals [t_M, t_M + delta_t], such that t_M is an original MIST age
+ind_orig = np.where(t_orig)[0]
+t_new = (t[ind_orig[:-1] + 1][:7] + t[ind_orig][:7]) / 2
+t = np.sort(np.concatenate((t, t_new)))
 # non-rotating models at these ages and full mass range
 stc = st.copy(); stc.select(stc.omega0 == 0)  
 

config.py
@@ -101,9 +101,13 @@ if mix: # the enhanced mixing analysis
 else: # the MIST analysis
 	n = 21 # number of steps in each dimension
 	tmin, tmax = [9.154, 9.165] # age
-	smin, smax = [0.036, 0.047] # sigma_age
-	w0min, w0max = [0.025, 0.225] # slow proportion, age spread
-	w1min, w1max = [0.4, 0.9] # fast proportion, age spread
+	smin, smax = [0.01, 0.03] # sigma_age
+	w0min, w0max = [0, 0.15] # slow proportion
+	w2min, w2max = [0, 0.3] # fast proportion
+	# tmin, tmax = [9.154, 9.165] # age
+	# smin, smax = [0.036, 0.047] # sigma_age
+	# w0min, w0max = [0.025, 0.225] # slow proportion
+	# w2min, w2max = [0.4, 0.9] # fast proportion
 	# rotational proportion grids (age parameter grids determined elsewhere)
 	w0 = np.linspace(w0min, w0max, n, dtype=float) 
-	w1 = np.linspace(w1min, w1max, n, dtype=float)
+	w2 = np.linspace(w2min, w2max, n, dtype=float)

lib/dens_util.py
@@ -60,7 +60,8 @@ class Kernel:
 		self.n = n
 
 # run this function with the first convolved prior as the argument
-# to obtain the kernels and corresponding slices necessary for individual-star error integrations
+# to obtain the kernels and corresponding slices necessary for individual-star error integrations;
+# the kernels are normalized to sum to 1
 def calc_kernels(density, sigma, nsig):
 	npts = ld.obs.shape[0] # number of data points
 	ndim = ld.obs.shape[1] # number of observable dimensions
@@ -103,7 +104,6 @@ def calc_kernels(density, sigma, nsig):
 					kernel = kernel_j
 				else: 
 					kernel = np.multiply.outer(kernel, kernel_j)
-		# kernel /= np.sum(kernel)
 		kernels.append(kernel)
 		slices.append( tuple(slc) )
 	return [kernels, slices]

plot/plot_densities.py
@@ -47,15 +47,16 @@ def plot(density, cmap, textstr, j, plot_type, base):
 		# # mask for scatter that depends on rotational population
 		# mask = (ld.vsini >= cf.vsini_bins[j]) & (ld.vsini < cf.vsini_bins[j+1])
 
-		dens = density.density();
+		dens = density.density() # this version integrates to 1 over RON, instead of summing to 1
 		dens[dens == 0] = 1e-300
 		dens = np.log(dens)
 		norm = mpl.colors.Normalize(vmin=cmap_min, vmax=cmap_max, clip=True)
 
 		fig, [ax, ax1] = plt.subplots(ncols=2, gridspec_kw={'width_ratios': [5, 1]})
 		plt.subplots_adjust(left=0.13, right=0.8, bottom=0.13, top=0.95)
+		# interpolated shading for smoother plot; for true density values, use shading='nearest' 
 		pcm = ax.pcolormesh(density.obs[1], density.obs[0], dens, cmap=cmap, norm=norm,\
-			shading='nearest') 
+			shading='gouraud') 
 		ax.set_xlim(left=ld.obmin_plot[xi], right=ld.obmax_plot[xi])
 		ax.set_ylim(bottom=ld.obmin_plot[0], top=ld.obmax_plot[0])
 		ax.invert_yaxis()
@@ -112,7 +113,7 @@ for filepath in filelist:
 			# VM density
 			density_vsini = density.copy()
 			density_vsini.marginalize(1)
-			density_vsini.normalize()
+			density_vsini.normalize() # so that the sum over the RON is 1
 			densities_vsini[j][k][it] = density_vsini
 
 			## text 
@@ -146,11 +147,16 @@ for filepath in filelist:
 ## plot the minimum-error density at maximum-likelihood cluster age parameters
 # define a prior in age
 t = np.array(t)
+# age steps for mid-point Riemann sum integration; dimension: age
+delta_t = np.concatenate((np.array([t[1] - t[0]]), (t[2:] - t[:-2])/2, np.array([t[-1] - t[-2]])))
+# parameters of the prior and the prior itself
 t_mean = 9.1589
 t_std = 0.0414
 t_pr = np.exp( -0.5 * (t - t_mean)**2 / t_std**2 )
-t_pr /= np.sum(t_pr) # normalize
-# t_pr /= (t[1] - t[0]) # normalize so that the integral is 1
+# assume integration is Riemann, with the given delta ages; multiply by the deltas
+t_pr *= delta_t
+# normalize so that the prior adds up to 1
+t_pr /= np.sum(t_pr) 
 for j in range(len(densities_cmd)):
 	for k in range(len(densities_cmd[j])):
 		for it in range(len(densities_cmd[j][k])):

plot/plot_ll.py
@@ -71,7 +71,7 @@ filelist = list(np.sort(glob.glob(like_dir + 'pkl/ll*.pkl')))
 for filepath in filelist: # for each combination of age and metallicity
 	# load the log likelihood and the maximum q
 	with open(filepath, 'rb') as f:
-		ll_4d, qm_4d, bm_4d, t0_ar, t1_ar, w0, w1, om_sigma = pickle.load(f)
+		ll_4d, qm_4d, bm_4d, t0_ar, t1_ar, w0, w2, om_sigma = pickle.load(f)
 	# get base file name
 	base = os.path.basename(filepath).split('.')[0]
 	if len(base.split('p')[-1]) == 1:
@@ -79,7 +79,7 @@ for filepath in filelist: # for each combination of age and metallicity
 	# set maximum likelihood to zero
 	ll_4d -= np.nanmax(ll_4d) 	
 	# indices of ML parameters
-	w0m, w1m, t0m, t1m = np.unravel_index(np.nanargmax(ll_4d), ll_4d.shape)  
+	w0m, w2m, t0m, t1m = np.unravel_index(np.nanargmax(ll_4d), ll_4d.shape)  
 
 	# plot likelihood vs. rotational proportions, 
 	# at maximum-likelihood binaries proportion and age priors
@@ -91,27 +91,27 @@ for filepath in filelist: # for each combination of age and metallicity
 		r'$A_{\rm V}=' + '%.2f' % cf.A_V + '$',		
 		r'${\rm [M/H]}_{\rm MIST}=' + str(cf.Z) + '$',
 	    r'$\sigma_{\rm \omega} = \{' + ', '.join(['%.2f' % n for n in cf.om_sigma]) + '\}$',
-		r'$\widehat{w} = \{' + '%.2f' % w0[w0m] + ', ' + '%.2f' % (1 - w0[w0m] - w1[w1m]) +\
-			', ' + '%.2f' % w1[w1m] + '\}$',
+		r'$\widehat{w} = \{' + '%.2f' % w0[w0m] + ', ' + '%.2f' % (1 - w0[w0m] - w2[w2m]) +\
+			', ' + '%.2f' % w2[w2m] + '\}$',
 	    r'' + t0_label + ' = ' + t0_hat + ' = ' + '%.4f' % t0_ar[t0m],
 	    r'' + t1_label + ' = ' + t1_hat + ' = ' + '%.4f' % t1_ar[t1m],
-		r'$\widehat{q} = $' + '%.3f' % qm[w0m, w1m],
-		r'$\widehat{b} = $' + '%.2f' % bm[w0m, w1m]))
+		r'$\widehat{q} = $' + '%.3f' % qm[w0m, w2m],
+		r'$\widehat{b} = $' + '%.2f' % bm[w0m, w2m]))
 	filename = like_dir + 'png/' + base + '_rotation' + '.png'
-	plot(cf.w1, cf.w0, qm, bm, ll, r'$w_1$', r'$w_0$', textstr, filename)
+	plot(cf.w2, cf.w0, qm, bm, ll, r'$w_1$', r'$w_0$', textstr, filename)
 
 	# plot likelihood vs. age priors, 
 	# at maximum-likelihood binaries proportion and rotational population proportions
-	ll = ll_4d[w0m, w1m, ...] # log-likelihoods
-	qm = qm_4d[w0m, w1m, ...] # maximum q
-	bm = bm_4d[w0m, w1m, ...] # maximum b
+	ll = ll_4d[w0m, w2m, ...] # log-likelihoods
+	qm = qm_4d[w0m, w2m, ...] # maximum q
+	bm = bm_4d[w0m, w2m, ...] # maximum b
 	# text box text
 	textstr = '\n'.join((	
 		r'$A_{\rm V}=' + '%.2f' % cf.A_V + '$',	
 		r'${\rm [M/H]}_{\rm MIST}=' + str(cf.Z) + '$',
 		r'$\sigma_{\rm \omega} = \{' + ', '.join(['%.2f' % n for n in om_sigma]) + '\}$',
-		r'$w = \widehat{w} = \{' + '%.2f' % w0[w0m] + ', ' + '%.2f' % (1 - w0[w0m] - w1[w1m]) +\
-			', ' + '%.2f' % w1[w1m] + '\}$',
+		r'$w = \widehat{w} = \{' + '%.2f' % w0[w0m] + ', ' + '%.2f' % (1 - w0[w0m] - w2[w2m]) +\
+			', ' + '%.2f' % w2[w2m] + '\}$',
 	    t0_hat + ' = ' + '%.4f' % t0_ar[t0m],
 	    t1_hat + ' = ' + '%.4f' % t1_ar[t1m],
 		r'$\widehat{q} = $' + '%.3f' % qm[t0m, t1m],